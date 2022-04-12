64c5ef5389d40de95a2db3cde24d4dc4236c5871
apkcreateur <95294936+apkcreateur@users.noreply.github.com>
2022-04-12 21:51:08 +0100

    sec commit

---
+++ lib/util/theme_config.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ThemeConfig {
+  //Colors for theme
+  static Color lightPrimary = Color(0xfffcfcff);
+  static Color darkPrimary = Color(0xff1f1f1f);
+  static Color lightAccent = Colors.blue;
+  static Color darkAccent = Colors.lightBlue;
+  static Color lightBG = Color(0xfffcfcff);
+  static Color darkBG = Color(0xff121212);
+  static Color badgeColor = Colors.red;
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      elevation: 0, toolbarTextStyle: TextTheme(
+        headline6: TextStyle(
+          color: darkBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ).bodyText2, titleTextStyle: TextTheme(
+        headline6: TextStyle(
+          color: darkBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ).headline6,
+    ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: lightAccent),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    scaffoldBackgroundColor: darkBG,
+    appBarTheme: AppBarTheme(
+      elevation: 0, toolbarTextStyle: TextTheme(
+        headline6: TextStyle(
+          color: lightBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ).bodyText2, titleTextStyle: TextTheme(
+        headline6: TextStyle(
+          color: lightBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ).headline6,
+    ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: darkAccent),
+  );
+}
