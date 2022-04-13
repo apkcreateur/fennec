eba66159e2fa783a60e3088397935701c82930af
apkcreateur <95294936+apkcreateur@users.noreply.github.com>
2022-04-13 02:00:17 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'app.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fennec/util/theme_config.dart';
+import 'package:fennec/util/const.dart';
+import 'package:fennec/views/screens/auth/login.dart';
 
-void main() async {
+
+
+
+bool shouldUseFirestoreEmulator = false;
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  var DefaultFirebaseConfig;
+  await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
 
-  await Firebase.initializeApp();
-
+  if (shouldUseFirestoreEmulator) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Constants.appName,
+      theme: themeData(ThemeConfig.lightTheme),
+      // darkTheme: themeData(ThemeConfig.darkTheme),
+      home: Login(),
+    );
+  }
+
+  ThemeData themeData(ThemeData theme) {
+    return theme.copyWith(
+      textTheme: GoogleFonts.sourceSansProTextTheme(
+        theme.textTheme,
+      ),
+    );
+  }
 }
